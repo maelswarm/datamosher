aa1a5fee8accc0bed0d4de4228c3fd6ed12c23d7
Brandon Barber <roecrew@gmail.com>
2017-08-27 11:58:23 -0500

    0.1.6
    Added start, end, and repeat.

--- index.js
+++ index.js
@@ -20,35 +20,56 @@ var File = function (name) {
 	this.data = this.rawData.toString();
 };
 
-File.prototype.glitch = function(off, val, freq) {
+File.prototype.glitch = function(off, val, freq, repeat, start, end) {
 	if(val == undefined) {
 		val = 0;
+	}
+	if(repeat == undefined) {
+		repeat = 0;
 	}
 	if(off == undefined) {
 		off = 10000;
 	}
 	if(freq == undefined) {
-		freq = 0;
+		freq = 1;
+	}
+	if(start == undefined) {
+		start = 0;
+	}
+	if(end == undefined) {
+		end = 100;
+	}
+	
+	if(start >= end) {
+		console.log("Start value must be less than end value.");
+		return;
 	}
 	var x = 0;
 	if(this.fileType == "mp4") {
-		for(var i = getMPEGStart(this.data); i<this.rawData.length; i++) {
+		console.log(this.rawData.length);
+		for(var i = getMPEGStart(this.data)+Math.round(this.rawData.length*(start/100.0)); i<Math.round(this.rawData.length*(end/100.0)); i++) {
 			if(this.rawData[i] == 0 && this.rawData[i+1] == 0 && this.rawData[i+2] == 1 && x % freq == 0) {
-				this.rawData[i+off] = val;
+				for(var j=0;j<repeat;j++) {
+					this.rawData[i+off*(1+j)] = val;
+				}
 			}
 			x++;
 		}
 	} else if(this.fileType == "avi") {
-		for(var i = getAVIStart(this.data); i<this.rawData.length; i++) {
+		for(var i = getAVIStart(this.data)+Math.round(this.rawData.length*(start/100.0)); i<Math.round(this.rawData.length*(end/100.0)); i++) {
 			if(this.rawData[i] == 93 && x % freq == 0) {
-				this.rawData[i+off] = val;
+				for(var j=0;j<repeat;j++) {
+					this.rawData[i+off*(1+j)] = val;
+				}
 			}
 			x++;
 		}
 	} else if(this.fileType == "mkv") {
-		for(var i = 1000; i<this.rawData.length; i++) {
+		for(var i = Math.round(this.rawData.length*(start/100.0)); i<Math.round(this.rawData.length*(end/100.0)); i++) {
 			if(this.rawData[i] == 31 && this.rawData[i+1] == 67 && this.rawData[i+2] == 182 && this.rawData[i+3] == 117 && x % freq == 0) {
-				this.rawData[i+off] = val;
+				for(var j=0;j<repeat;j++) {
+					this.rawData[i+off*(1+j)] = val;
+				}
 			}
 			x++;
 		}
